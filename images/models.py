6adbdd38996e73bb4a1762f311f427b13c3d98e4
stratiiv <stratiyvalter@gmail.com>
2023-06-10 21:27:57 +0300

    add uploading images with previews generation

--- images/models.py
+++ images/models.py
@@ -1,3 +1,23 @@
+import os
 from django.db import models
+from .services import generate_preview_image
 
-# Create your models here.
+
+class Image(models.Model):
+    """Represents image entity."""
+    name = models.CharField(max_length=255)
+    image_type = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images')
+    preview = models.CharField(max_length=255, blank=True, null=True)  # URL for preview
+
+    def save(self, *args, **kwargs):
+        """Generate and save the preview image."""
+        super().save(*args, **kwargs)
+        preview_filename = f"preview_{os.path.basename(self.image.name)}"
+        generate_preview_image(self.image.path, preview_filename)
+        self.preview = os.path.join('images', 'previews',
+                                    preview_filename).replace("\\", "/")
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.image.name}"
---
+++ images/services.py
@@ -0,0 +1,27 @@
+import sys
+import os
+from PIL import Image
+from django.conf import settings
+
+
+PREVIEW_SIZE = (100, 100)
+
+# Add the parent directory to the Python module search path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+
+def generate_preview_image(image_path: str, preview_filename: str) -> None:
+    """Generate a preview image from the original image.
+
+    This function opens the original image, resizes it to the specified
+    preview size, and saves it as a PNG file in the previews directory.
+    """
+    with Image.open(image_path) as im:
+        im.thumbnail(PREVIEW_SIZE)
+        preview_path = os.path.join(settings.MEDIA_ROOT, 'images', 'previews',
+                                    preview_filename)
+        im.save(preview_path, "PNG")
+
+
+
