0a3a5edbc46f364f4f0191b9e27d560fa874cb1f
stratiiv <stratiyvalter@gmail.com>
2023-06-16 15:28:15 +0300

    provide permissions

--- images/views.py
+++ images/views.py
@@ -1,12 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from .serializers import ImageSerializer
 from .models import Image
+from .permissions import IsAuthorOrReadOnly
 
 
 class ImageViewSet(ModelViewSet):
     """Provides CRUD endpoints on Image"""
     serializer_class = ImageSerializer
     queryset = Image.objects.all()
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
         """Auto add author as a current user when creating"""
